089bd834fd6ddc09f2bd92b2ac778d39fc6e8bdd
Shawn Cicoria <github@cicoria.com>
2020-05-23 21:53:33 -0400

    adding eslint

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('hello world');
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.notFoundHandler = void 0;
+exports.notFoundHandler = (request, response, next) => {
+    const message = "Resource not found";
+    response.status(404).send(message);
+};
+//# sourceMappingURL=index.js.map
