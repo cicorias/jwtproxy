d6908bf0ffc10756a8019c9032d3050f844295a0
Shawn Cicoria <github@cicoria.com>
2020-05-23 21:32:24 -0400

    start implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('hello world')
+import { Request, Response, NextFunction } from "express";
+
+export const notFoundHandler = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+
+  const message = "Resource not found";
+
+  response.status(404).send(message);
+};
+
+
+/**
+ * append to the Express Response object
+ * new properties.
+ */
+declare namespace Express {
+  interface JwtVerify {
+      // Add JwtVerify properties in here
+  }
+
+  export interface Response {
+      jwtverify: JwtVerify
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,73 @@
+import express from 'express'
+import { Application, Request, Response, NextFunction, RequestHandler } from 'express'
+
+//https://dev.to/aligoren/developing-an-express-application-using-typescript-3b1
+
+const loggerMiddleware = (req: Request, resp: Response, next: NextFunction) => {
+  console.log('Request logged:', req.method, req.path);
+  next();
+}
+
+
+class App {
+    public app: Application
+    public port: number
+
+    constructor(appInit: { port: number; middleWares: RequestHandler[]; controllers: any; }) {
+        this.app = express()
+        this.port = appInit.port
+
+        this.middlewares(appInit.middleWares)
+        this.routes(appInit.controllers)
+        this.assets()
+        this.template()
+    }
+
+    private middlewares(middleWares: { forEach: (arg0: (middleWare: any) => void) => void; }) {
+        middleWares.forEach(middleWare => {
+            this.app.use(middleWare)
+        })
+    }
+
+    private routes(controllers: { forEach: (arg0: (controller: any) => void) => void; }) {
+        controllers.forEach(controller => {
+            this.app.use('/', controller.router)
+        })
+    }
+
+    private assets() {
+        this.app.use(express.static('public'))
+        this.app.use(express.static('views'))
+    }
+
+    private template() {
+        this.app.set('view engine', 'pug')
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the http://localhost:${this.port}`)
+        })
+    }
+}
+
+const app = new App({
+  port: 5000,
+  controllers: [
+      // new HomeController(),
+      // new PostsController()
+  ],
+  middleWares:  [
+      // bodyParser.json(),
+      // bodyParser.urlencoded({ extended: true }),
+      loggerMiddleware
+  ]
+})
+
+app.listen()
+
+
+
+
+
+
