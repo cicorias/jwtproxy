77fe707421e63926f6286af05dfbdd444edfde5b
Shawn Cicoria <github@cicoria.com>
2020-06-02 09:01:36 -0400

    cleanup and fix license

--- dist/server.d.ts
+++
@@ -1,4 +0,0 @@
-/** This is a demo server used to test and validate the actual middleware which is in index.ts */
-import express from 'express';
-declare const _default: express.Application;
-export default _default;
